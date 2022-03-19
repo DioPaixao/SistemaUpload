137314192ba37783d97ee0a63cc7ad3993349136
Diogo Paixão <69406859+DioPaixao@users.noreply.github.com>
2022-03-19 17:20:11 -0300

    update

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,6 @@
 const routes = require('express').Router()
 const multer = require('multer')
 const multerConfig = require('./config/multer')
-const { find } = require('./models/Post')
 
 const Post = require('./models/Post')
 
--- frontend/src/components/FileList/index.js
+++ frontend/src/components/FileList/index.js
@@ -1,40 +1,54 @@
-import { Container, FileInfo, Preview } from './styles'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import { MdCheckCircle, MdError, MdLink } from 'react-icons/md'  
+import { Container, FileInfo, Preview } from './styles'
 
-const FileList = () => (
+const FileList = ({ files, onDelete }) => (
 	<Container>
-		<li>
-			<FileInfo>
-				<Preview src="https://sistemaupload.s3.amazonaws.com/e01a6fde92cd10e19449a94c37c44e87-profile.jpg" />
-				<div>
-					<strong>profile.png</strong>
-					<span>64kb <button onClick={() => {}}>Excluir</button></span>
-				</div>
-			</FileInfo>
-
+	  {files.map(uploadedFile => (
+		<li key={uploadedFile.id}>
+		  <FileInfo>
+			<Preview src={uploadedFile.preview} />
 			<div>
-				<CircularProgressbar
-					styles={{
-						root: { width: 24 },
-						path: { stroke: '#7159c1' }
-					}}
-					strokeWidth={10}
- 					value={60}
-				/>
-				<a 
-					href='https://sistemaupload.s3.amazonaws.com/e01a6fde92cd10e19449a94c37c44e87-profile.jpg'
-					target="_blank"
-					rel='noopener noreferrer'
-				>
-					<MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-				</a>
-
-				<MdCheckCircle size={24} color="#78e5d5" />
-				<MdError size={24} color="#e57878" />
+			  <strong>{uploadedFile.name}</strong>
+			  <span>
+				{uploadedFile.readableSize}{" "}
+				{!!uploadedFile.url && (
+				  <button onClick={() => onDelete(uploadedFile.id)}>
+					Excluir
+				  </button>
+				)}
+			  </span>
 			</div>
+		  </FileInfo>
+  
+		  <div>
+			{!uploadedFile.uploaded &&
+			  !uploadedFile.error && (
+				<CircularProgressbar
+				  styles={{
+					root: { width: 24 },
+					path: { stroke: "#7159c1" }
+				  }}
+				  strokeWidth={10}
+				  percentage={uploadedFile.progress}
+				/>
+			  )}
+  
+			{uploadedFile.url && (
+			  <a
+				href={uploadedFile.url}
+				target="_blank"
+				rel="noopener noreferrer"
+			  >
+				<MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+			  </a>
+			)}
+  
+			{uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+			{uploadedFile.error && <MdError size={24} color="#e57878" />}
+		  </div>
 		</li>
+	  ))}
 	</Container>
-)
-
+  );
 export default FileList
